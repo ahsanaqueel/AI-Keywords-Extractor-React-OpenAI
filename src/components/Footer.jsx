f0c34f82198f73c96324296a4fd6b9323da4b128
ahsan aqueel <ahsansunny08@gmail.com>
2023-04-07 10:13:45 +0500

    code Added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Box, Image, Flex,Text } from '@chakra-ui/react'
+import logo from '../assets/openai.png';
+const Footer = () => {
+  return (
+    <Box marginTop={50}>
+        <Flex justifyContent='center' alignItems='center'>
+            <Image src={logo} marginRight={1}/>
+            <Text>
+                Powered By Open AI
+            </Text>
+        </Flex>
+    </Box>
+  )
+}
+
+export default Footer
